ab1877b1d6ee7dfe2777c605c888395441524b46
Bbolive <rapatsara.bam@gmail.com>
2025-03-06 13:31:02 +0700

    flutter_app_37_end

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart'; // นำเข้า Google Fonts
+import 'package:flutter_app_6519410037/views/register_ui.dart';
+import 'package:flutter/services.dart'; // นำเข้าเพื่อใช้ FilteringTextInputFormatter
 
 class LoginUi extends StatefulWidget {
   const LoginUi({super.key});
@@ -11,41 +15,260 @@ class _LoginUiState extends State<LoginUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo_flutter.png',
-              width: MediaQuery.of(context).size.width * 0.5,
-            ),
-            Text(
-              'Welcome to FLUTTER',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.035,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 212, 205, 205),
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: 35.0,
+          right: 35.0,
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.075,
               ),
-            ),
-            Text(
-              'DESIGN YOUR LIFE',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.02,
-                color: Colors.white,
+              Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/logo_flutter.png',
+                  width: MediaQuery.of(context).size.width * 0.45,
+                ),
               ),
-            ),
-            Text(
-              'DESIGN YOUR FUTURE',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.02,
-                color: Colors.white,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
-            ), 
-          ],
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Welcome to FLUTTER',
+                  style: GoogleFonts.kanit(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'DESIGN YOUR LIFE',
+                  style: GoogleFonts.kanit(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'DESIGN YOUR FUTURE',
+                  style: GoogleFonts.kanit(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              TextField(
+                style: GoogleFonts.kanit(), // กำหนดฟอนต์ใน TextField
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter
+                      .digitsOnly, // จำกัดให้รับเฉพาะตัวเลข
+                ],
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.person_2_outlined,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนรหัสนักศึกษา',
+                  hintStyle: GoogleFonts.kanit(), // กำหนดฟอนต์ใน hintText
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.015,
+              ),
+              TextField(
+                style: GoogleFonts.kanit(), // กำหนดฟอนต์ใน TextField
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      width: 2.0,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนรหัสผ่าน',
+                  hintStyle: GoogleFonts.kanit(), // กำหนดฟอนต์ใน hintText
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.015,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot Password?",
+                    style: GoogleFonts.kanit(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.015,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                // ignore: sort_child_properties_last
+                child: Text(
+                  "Login",
+                  style: GoogleFonts.kanit(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(200.0, 50.0),
+                  backgroundColor: const Color.fromARGB(255, 26, 35, 88),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.015,
+              ),
+              Text(
+                "Or login with",
+                style: GoogleFonts.kanit(),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.015,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // ฟังก์ชันสำหรับ Facebook login
+                    },
+                    icon: FaIcon(
+                      FontAwesomeIcons.facebook,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      "Facebook",
+                      style: GoogleFonts.kanit(
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // ฟังก์ชันสำหรับ Google login
+                    },
+                    icon: FaIcon(
+                      FontAwesomeIcons.google,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      "Google",
+                      style: GoogleFonts.kanit(
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.035,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: GoogleFonts.kanit(),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RegisterUi(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Sign up',
+                      style: GoogleFonts.kanit(
+                        color: Colors.blueAccent,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                'Create by 6519410037',
+                style: GoogleFonts.kanit(),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/views/register_ui.dart
+++ lib/views/register_ui.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_6519410037/views/login_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart'; // นำเข้าเพื่อใช้ FilteringTextInputFormatter
 
 class RegisterUi extends StatefulWidget {
   const RegisterUi({super.key});
@@ -11,41 +14,314 @@ class _RegisterUiState extends State<RegisterUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400] ,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo_flutter.png',
-              width: MediaQuery.of(context).size.width * 0.5,
-            ),
-            Text(
-              'Welcome to FLUTTER',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.035,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 212, 205, 205),
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: 35.0,
+          right: 35.0,
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
               ),
-            ),
-            Text(
-              'DESIGN YOUR LIFE',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.02,
-                color: Colors.white,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: MediaQuery.of(context).size.height * 0.045,
+                  ),
+                ),
               ),
-            ),
-            Text(
-              'DESIGN YOUR FUTURE',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.02,
-                color: Colors.white,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
-            ), 
-          ],
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Let\'s Get Started!',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 0.038,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Kanit', // กำหนดฟอนต์เป็น Kanit
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Create new account for Flutter Dev.',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 0.017,
+                    fontFamily: 'Kanit', // กำหนดฟอนต์เป็น Kanit
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.055,
+              ),
+              TextField(
+                style: GoogleFonts.kanit(), // กำหนดฟอนต์ใน TextField
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter
+                      .digitsOnly, // จำกัดให้รับเฉพาะตัวเลข
+                ],
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.person_2_outlined,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนรหัสนักศึกษา',
+                  hintStyle: GoogleFonts.kanit(), // กำหนดฟอนต์ใน hintText
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              // Repeat this for other TextFields
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // กำหนดความโค้งมน
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // กำหนดความโค้งมน
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนอีเมล์',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                  ),
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              // Continue for other TextFields with fontFamily: 'Kanit'
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              TextField(
+                keyboardType: TextInputType.phone, // ใช้สำหรับเบอร์โทรศัพท์
+                inputFormatters: [
+                  FilteringTextInputFormatter
+                      .digitsOnly, // จำกัดให้รับเฉพาะตัวเลข
+                ],
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.phone,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนเบอร์โทรศัพท์',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                  ),
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              // Continue for other TextFields with fontFamily: 'Kanit'
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              TextField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // กำหนดความโค้งมน
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // กำหนดความโค้งมน
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนรหัสผ่าน',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                  ),
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              // Continue for other TextFields with fontFamily: 'Kanit'
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.025,
+              ),
+              TextField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // กำหนดความโค้งมน
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.circular(30.0), // กำหนดความโค้งมน
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.blueAccent,
+                  ),
+                  hintText: 'ป้อนยืนยันรหัสผ่าน',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                  ),
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                ),
+              ),
+              // Continue for other TextFields with fontFamily: 'Kanit'
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.055,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "REGISTER",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(
+                    200.0,
+                    50.0,
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 26, 35, 88),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      30.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.055,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginUi(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Login here',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Kanit', // ใช้ฟอนต์ Kanit
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
